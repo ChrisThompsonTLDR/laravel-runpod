a0e09fcc3a6ac2eb76c631fd93c2f6dda75e8951
Chris <chris@example.com>
2026-02-28 21:40:16 -0700

    Add StorageMacros placeholder (referenced by registerMacros)

---
+++ src/Macros/StorageMacros.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ChrisThompsonTLDR\LaravelRunPod\Macros;
+
+class StorageMacros
+{
+    public static function register(): void
+    {
+        // Placeholder for future Storage macro registrations
+    }
+}
